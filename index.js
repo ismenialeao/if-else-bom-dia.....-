181d4592e66e04fd7a01d7c0da2a889e3cd14f8c
@ismyleao <63822305+ismenialeao@users.noreply.github.com>
2021-06-24 20:39:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//iF Else
+//se estiver entre 6:00 e 12:00: bom dia!
+//se estiver entre 12:00 as 18:00 :boa tarde!
+// caso contrario: boa noite!
+
+let hora = 19
+
+if (hora > 6 && hora < 12) { 
+console.log('Bom dia!') 
+}
+
+else if (hora > 12 && hora < 18 ) {  
+console.log('Boa tarde!') 
+}
+
+else{
+console.log('Boa noite!')
+
+}
